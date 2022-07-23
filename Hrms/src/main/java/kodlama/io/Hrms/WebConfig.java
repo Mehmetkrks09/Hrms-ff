168ccd05855190da986305f8903ad20ba3e01eb4
Mehmetkrks09 <mehmetkarakas415@gmail.com>
2022-07-24 00:53:00 +0300

    sdfs

--- Hrms/src/main/java/kodlama/io/Hrms/WebConfig.java
+++ Hrms/src/main/java/kodlama/io/Hrms/WebConfig.java
@@ -1,16 +1,16 @@
 package kodlama.io.Hrms;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-}  
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://hrms-java.herokuapp.com");
+			}
+		};
+	}
+}
